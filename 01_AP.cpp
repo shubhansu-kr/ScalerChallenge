417cb71c1ed00e25259d212beebbefac8d7486ee
Shubhansu kumar <shubhansu2021@gmail.com>
2022-09-28 12:37:54 +0530

    Day2
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_AP.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int d = arr[1] - arr[0];
+        for (int i = 2; i < arr.size(); ++i)
+        {
+            if (arr[i] - arr[i-1] != d) return false;
+        }
+        return true;
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
