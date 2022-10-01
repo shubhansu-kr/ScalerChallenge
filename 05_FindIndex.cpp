99ca4af3ea010cc586823fdb537ed19e7df24df1
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-01 21:43:45 +0530

    Find index
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 05_FindIndex.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        int i = findIndex(nums, target), j = findIndex(nums, target + 1) - 1;
+        if (i < nums.size() && nums[i] == target) return {i, j};
+        else return {-1, -1};
+    }
+
+    int findIndex(vector<int> &nums, int target)
+    {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r)
+        {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] < target)l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
