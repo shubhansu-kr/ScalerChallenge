50a884e8664d7e31e9c347c52185141802104556
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-18 19:43:49 +0530

    Valid Parenthesis
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 22_ValidParenthesis.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> par; 
+        for(auto &it: s)
+        {
+            if (it == '(' || it == '{' || it == '[') {
+                par.push(it);
+            }
+            else {
+                if (!par.size()) return false;
+                if (it == ')' && par.top() != '(' || it == '}' && par.top() != '{' || it == ']' && par.top() != '[') return false;
+
+                par.pop();
+            }
+        }
+        return par.size() == 0;
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
